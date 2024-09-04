d024e43d257678b20d0e88c62d67f976593c618a
YilingChen <shutuzi88@gmail.com>
2024-09-04 19:46:15 +0800

    deleted:    .vscode/settings.json deleted:   js/webpack/es6.custom.dialog.bundle1.0.0.js.backup deleted:   js/webpack/es6.custom.dialog.bundle1.0.0.js.map modified:   webpack.config.js

--- HTMLDialogElement/webpack.config.js
+++ HTMLDialogElement/webpack.config.js
@@ -1,22 +1,22 @@
 const path = require('path');
-const package = require('./package.json'); // 引入package.json文件
+const {name, version} = require('./package.json'); // 引入package.json文件
 
 module.exports = {
   mode: 'production', //development
-  entry: `./js/${package.name}${package.version}.js`, // 动态设置入口文件
+  entry: `./js/${name}${version}.js`, // 動態設定入口文件
   output: {
     path: path.resolve(__dirname, './js/webpack'),
-    filename: `${package.name}.bundle${package.version}.js`, // 动态设置输出文件名
+    filename: `${name}.bundle${version}.js`, // 動態設定輸出檔名
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // 使用正则表达式匹配需要转换的文件
-        exclude: /node_modules/, // 排除不需要转换的文件
+        test: /\.js$/, // 使用正規表示式來匹配需要轉換的文件
+        exclude: /node_modules/, // 排除不需要轉換的文件
         use: {
-          loader: 'babel-loader', // 使用 babel-loader 进行转换
+          loader: 'babel-loader', // 使用 babel-loader 進行轉換
           options: {
-            presets: ['@babel/preset-env'], // 使用 @babel/preset-env 进行转换
+            presets: ['@babel/preset-env'], // 使用 @babel/preset-env 進行轉換
             // plugins: ['@babel/plugin-transform-modules-commonjs'] // 添加插件配置
           }
         }
